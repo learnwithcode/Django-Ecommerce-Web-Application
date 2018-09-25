2ea950c2e68014aec0aafe6c966994ef0f1a6bd1
Abhiraj Pundir <pundirabhiraj@gmai.com>
2018-09-25 10:44:48 +0530

    Django Admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Product
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'price', 'created','updated']
+    search_fields = ['__str__', 'price']
+    list_editable = ['price']
+    list_filter = ['price', 'created', 'updated']
+admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -6,6 +6,8 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
