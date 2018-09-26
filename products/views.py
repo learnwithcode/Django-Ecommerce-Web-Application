21dcb5f7bcee66f836d1205f57c9076ae2849add
Abhiraj Pundir <pundirabhiraj@gmai.com>
2018-09-26 15:25:19 +0530

    Detail View

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 # Create your views here.
 
@@ -9,7 +9,6 @@ class ProductListView(ListView):
 
     def get_context_data(self):
         context = super(ProductListView, self).get_context_data()
-        print(context)
         return context
 
 
@@ -20,3 +19,20 @@ def product_list_view(request):
     }
     return render(request, "products/list.html", context)
 
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        #print(context)
+        return context
+
+
+def product_detail_view(request, pk=None):
+    instance = get_object_or_404(Product, pk=pk)
+    context = {
+        'object': instance
+    }
+    return render(request, "products/detail.html", context)    
